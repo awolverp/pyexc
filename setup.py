15cf5fe492b3b605910b4637f0693016af68d0be
aWolverP <118073811+awolverp@users.noreply.github.com>
2022-12-03 11:48:35 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,28 @@ import os.path
 
 PLACE = os.path.dirname(os.path.abspath(__file__))
 
+with open("README.md", "r") as f:
+    data = f.read()
+
 setup(
     name="pyexc",
     version="1.0.0a0",
     description="PyExc - Python C Extension to Manage Exceptions ...",
     author="awolverp",
+    long_description=data,
+    long_description_content_type = "text/markdown",
+    project_urls = {
+        "Github": "https://github.com/awolverp/pyexc/",
+        "Bug Tracker": "https://github.com/awolverp/pyexc/issues/new",
+    },
+    classifiers = [
+        "Natural Language :: English",
+        "Natural Language :: Persian",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU GPLv3",
+    ],
     data_files=[("", [PLACE+"/pyexc/pyexc.pyi"])],
     ext_modules=[
         Extension(
