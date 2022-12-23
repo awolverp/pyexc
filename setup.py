362a67872399a865e23d98f8908ec39cdb5e61e7
aWolverP <118073811+awolverp@users.noreply.github.com>
2022-12-23 11:57:03 +0330

    Try to setup pyi file

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 from setuptools import (setup, Extension)
-import os.path
+# import os.path
 
-BASE = os.path.dirname(os.path.abspath(__file__))
-
-join_path = lambda x: os.path.join(BASE, *x)
-
-with open(join_path(["README.md"]), "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-STUBS = join_path(["pyexc"])
+ext = Extension(
+    "pyexc", sources=["pyexc.cpp"]
+)
 
 setup(
     name="pyexc",
     version="1.0.0",
+    
     description="Python exception manager written in C++.",
     long_description=long_description,
     long_description_content_type = "text/markdown",
+    
     author="awolverp",
     author_email="awolverp@gmail.com",
-    url="https://github.com/awolverp/pyexc",
+    
+    url="https://github.com/awolverp/pyexc/",
+
+    packages=["pyexc-stubs"],
+    package_data={"pyexc-stubs": ["__init__.pyi"]},
     include_package_data=True,
-    packages=[STUBS],
-    package_data={STUBS: ["__init__.pyi"]},
+    
     ext_modules=[
-        Extension(
-            "pyexc",
-            sources=[join_path(["src", "pyexc.cpp"])],
-            language="c++"
-        )
+        ext    
     ],
+    
     project_urls = {
         "Github": "https://github.com/awolverp/pyexc/",
         "Bug Tracker": "https://github.com/awolverp/pyexc/issues/new",
