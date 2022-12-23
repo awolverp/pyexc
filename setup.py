4901c2b9fdb842aeb4b334c64c5090e2916ddb05
aWolverP <118073811+awolverp@users.noreply.github.com>
2022-12-23 11:42:52 +0330

    Try to setup pyi files

--- setup.py
+++ setup.py
@@ -8,20 +8,24 @@ join_path = lambda x: os.path.join(BASE, *x)
 with open(join_path(["README.md"]), "r") as f:
     long_description = f.read()
 
+STUBS = join_path(["pyexc-stubs"])
+
 setup(
     name="pyexc",
     version="1.0.0",
-    description="PyExc - Python exception manager written in C++.",
+    description="Python exception manager written in C++.",
     long_description=long_description,
     long_description_content_type = "text/markdown",
     author="awolverp",
     author_email="awolverp@gmail.com",
     url="https://github.com/awolverp/pyexc",
-    data_files=[(".", [join_path(["pyexc", "pyexc.pyi"])])],
+    include_package_data=True,
+    packages=[STUBS],
+    package_data={STUBS: [join_path(["pyexc-stubs", "__init__.pyi"])]},
     ext_modules=[
         Extension(
             "pyexc",
-            sources=[join_path(["pyexc", "pyexc.cpp"])],
+            sources=[join_path(["src", "pyexc.cpp"])],
             language="c++"
         )
     ],
